27ac075f5e65fe623890bd2f4806c9e63b8f00e5
Marcus <69986201+marcus-hill@users.noreply.github.com>
2024-11-03 14:15:57 +0000

    Complete project

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById("open-share-mobile").addEventListener('click', function(event) {
+    document.getElementById("author-section-mobile").classList.add("hidden");
+    document.getElementById("share-section").classList.remove("hidden");
+});
+
+document.getElementById("close-share").addEventListener('click', function(event) {
+    document.getElementById("author-section-mobile").classList.remove("hidden");
+    document.getElementById("share-section").classList.add("hidden");
+});
+
+document.getElementById("open-share-desktop").addEventListener('click', function(event) {
+    document.getElementById("share-section").classList.toggle("hidden");
+});
